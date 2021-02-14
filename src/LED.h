3dce1ca8fcde4c09b56f7cb3d11f15e0938c3e3f
NanduPathak <meet2nandu@gmail.com>
2021-02-14 16:57:12 +0530

    ESP32_OTA

---
+++ src/LED.h
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <ArduinoOTA.h>
+#include<ESP32-OTA.h>
+
+#include <lvgl.h>
+#include <TFT_eSPI.h>
+#include "FS.h"
+#include "SPI.h"
+#include "SD.h"
+#include "PCF8574.h"
+#include "FastLED.h" 
+
+File bmpFile;
+File root;
+String m_CurrentFilename = "";
+String m_CurrentFoldername = "";
+int m_FileIndex = 0;
+int m_FolderIndex = 0;
+int m_NumberOfFiles = 0;
+int m_NumberOfFolder = 0;
+String m_FileNames[300];
+String m_FolderName[300];
